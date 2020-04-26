e284b3fe0a31f8502fb9f5a0575d848851d8aebf
alessandrofonseca <alessandrofonseca@192.168.0.4>
2020-04-25 22:56:16 -0300

    feito a leitura das linhas do arquivo,e feito descarte dos comentários, planejamento do futuro sombrio que me aguarda....

--- Montador_PH1/Sources/main.c
+++ Montador_PH1/Sources/main.c
@@ -6,10 +6,16 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc,char *argv[]){
 
 	FILE * arq;
+	char str[100];// tam buffer temp leitura
+	char buff[100][100];// buffer definitivo
+	int linha=0;// saber qual linha
+	int i;// for
+
 	arq = fopen("programa.txt", "r");
 	if(arq == NULL) /// se nao conseguiu abrir o arquivo...
 	{
@@ -17,7 +23,21 @@ int main(int argc,char *argv[]){
 		return 0;
 	}
 
-
+	while(!feof(arq)){
+		if(fgets(str,100,arq)!=NULL){
+			//printf("%s", str);
+			for(i=0;i<100;i++){// desartar o comentario, tudo após o ;
+				if(str[i]==';' || str[i] == '\n' || str[i] == '\0'){//testar se o char é ; ou se um \n ou \0 que e o fim do string,
+					strncpy (&buff[linha][0],str,i);// copia o que está antes do ';'
+					buff[linha][i] = '\0';//adicionar manualmente o terminador de string pois o strncpy nao faz isso
+					printf("%s\n", &buff[linha][0]);
+					linha++;
+					break;// sai fora do for quando encontrar o ;
+				}
+			}
+		}
+	}
+	fclose(arq);
 	printf("Hello World\n");
 	return 0;
 }
